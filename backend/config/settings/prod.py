5c3719fe2c7dc45ea74a849d77062384b4b4d19f
Adriano Rodrigues <adrianordp1@gmail.com>
2026-03-05 15:15:45 -0400

    feat(backend/config/settings/prod.py): add production settings for Django with email and security configurations

---
+++ backend/config/settings/prod.py
@@ -0,0 +1,23 @@
+from .base import BASE_DIR, env
+
+DEBUG = False
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+
+# HTTPS
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
