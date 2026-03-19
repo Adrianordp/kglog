fab761182beb02bb8a9aaee9097f5a89f978e91e
Adriano Rodrigues <adrianordp1@gmail.com>
2026-03-19 18:04:21 -0400

    refactor(backend/app/schemas/body_composition.py): rename user_id field to id_user in BodyCompositionRead schema

--- backend/app/schemas/body_composition.py
+++ backend/app/schemas/body_composition.py
@@ -98,7 +98,7 @@ class BodyCompositionRead(BodyCompositionBase):
     id: int = Field(
         ..., description="Unique identifier for the body composition record"
     )
-    user_id: int = Field(
+    id_user: int = Field(
         ...,
         description="Identifier of the user to whom the body composition record belongs",
     )
@@ -127,7 +127,7 @@ class BodyCompositionRead(BodyCompositionBase):
         "schema_extra": {
             "example": {
                 "id": 1,
-                "user_id": 1,
+                "id_user": 1,
                 "measure_date": "2024-01-01T08:00:00",
                 "weight": 70.0,
                 "fat_percentage": 0.15,
