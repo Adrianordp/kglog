fdee8da6b8990562c3cfc17b62f6be189b87ad60
Adriano Rodrigues <adrianordp1@gmail.com>
2026-03-19 17:28:32 -0400

    feat(backend/app/models/user.py): add created_at and updated_at fields to User model
    Enhances the User model by including timestamps for record creation and updates.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,14 +6,16 @@ This model represents a user, including:
 - password hash
 - date of birth
 - gender
+- created at
+- updated at
 
 This model is used to store and manage user information
 """
 
-from datetime import date
+from datetime import date, datetime, timezone
 from enum import Enum as PyEnum
 
-from sqlalchemy import Date, Enum, Integer, String
+from sqlalchemy import Date, DateTime, Enum, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
@@ -31,8 +33,19 @@ class User(Base):
     id: Mapped[int] = mapped_column(
         Integer, primary_key=True, autoincrement=True
     )
+
     username: Mapped[str] = mapped_column(String(50), unique=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password_hash: Mapped[str] = mapped_column(String(128))
     date_of_birth: Mapped[date] = mapped_column(Date)
     gender: Mapped[Gender] = mapped_column(Enum(Gender))
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
