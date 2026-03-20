482ec2481aad0730f72492fa46fe2be5bcf6ff18
Adriano Rodrigues <adrianordp1@gmail.com>
2026-03-19 20:08:37 -0400

    test(backend/tests/unit/test_body_composition_model.py): add unit test for BodyComposition model

---
+++ backend/tests/unit/test_body_composition_model.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timezone
+
+from app.models.body_composition import BodyComposition
+
+
+def test_body_composition_model():
+    data = {
+        "id": 1,
+        "id_user": 1,
+        "measure_date": datetime(2024, 1, 1, 0, 0, 0, tzinfo=timezone.utc),
+        "weight": 70.0,
+        "fat_percentage": 0.15,
+        "muscle_percentage": 0.40,
+        "bone_percentage": 0.05,
+        "water_percentage": 0.60,
+    }
+
+    body_composition = BodyComposition(**data)
+
+    assert body_composition.id == data["id"]
+    assert body_composition.id_user == data["id_user"]
+    assert body_composition.measure_date == data["measure_date"]
+    assert body_composition.weight == data["weight"]
+    assert body_composition.fat_percentage == data["fat_percentage"]
+    assert body_composition.muscle_percentage == data["muscle_percentage"]
+    assert body_composition.bone_percentage == data["bone_percentage"]
+    assert body_composition.water_percentage == data["water_percentage"]
