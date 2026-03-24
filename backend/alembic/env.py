f22c8ec9ec07e736125ba3c6f1abfbc7b7ea3e98
Adriano Rodrigues <adrianordp1@gmail.com>
2026-03-24 12:34:34 -0400

    feat(backend/alembic/env.py): configure Alembic with database URL and target metadata
    Sets the SQLAlchemy URL for Alembic migrations and updates target metadata to use the Base model.

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,10 +3,18 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from app.core.settings import settings
+from app.models.base import Base
+from app.models.user import User  # noqa: F401
+
+DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = DATABASE_URL.replace("+asyncpg", "")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
